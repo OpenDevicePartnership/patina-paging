cf81150dbd25f5eb77cb073d93d899577f5a5475
Kun Qin <kuqin@microsoft.com>
2025-01-28 17:28:53 -0800

    Do not check this commit in!!!

--- src/aarch64/replace_table_entry.asm
+++ src/aarch64/replace_table_entry.asm
@@ -11,32 +11,6 @@
 .global replace_live_xlat_entry
 .global replace_live_xlat_entry_size
 
-  .set CTRL_M_BIT,      (1 << 0)
-
-  .macro __replace_entry, el
-
-.L1_\@:
-  # write invalid entry
-  str   xzr, [x0]
-  dsb   nshst
-
-  # flush translations for the target address from the TLBs
-  lsr   x2, x2, #12
-  .if   \el == 1
-  tlbi  vaae1, x2
-  .else
-  tlbi  vae\el, x2
-  .endif
-  dsb   nsh
-
-  # write updated entry
-  str   x1, [x0]
-  dsb   nshst
-  isb
-
-.L2_\@:
-  .endm
-
 # fn replace_live_xlat_entry(entry_ptr: u64, val: u64, addr: u64);
 #
 # Align this routine to a log2 upper bound of its size, so that it is
@@ -52,18 +26,21 @@ replace_live_xlat_entry:
   msr   daifset, #0xf
   isb
 
-  mrs    x5, CurrentEL
-  cmp    x5, #0x8
-  b.gt   .
-  b.eq   2f
-  cbnz   x5, 1f
-1:__replace_entry 1
-  b     4f
-2:__replace_entry 2
-  b     4f
-3:__replace_entry 3
+  # write invalid entry
+  str   xzr, [x0]
+  dsb   nshst
 
-4:msr   daif, x4
+  # flush translations for the target address from the TLBs
+  lsr   x2, x2, #12
+  tlbi  vae2, x2
+  dsb   nsh
+
+  # write updated entry
+  str   x1, [x0]
+  dsb   nshst
+  isb
+
+  msr   daif, x4
   ret
 
 replace_live_xlat_entry_size:
